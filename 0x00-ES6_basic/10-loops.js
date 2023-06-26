416a297c1e7e9f25b5b0f8fa083715e15be03458
Moses-Onche <onchemoses38@gmail.com>
2023-06-27 00:02:44 +0100

    Rewrite for loop using ES6 standards

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (let idx in array) {
-    let value = array[idx];
-    array[idx] = appendString + value;
+    let value = idx;
+    idx = appendString + value;
   }
 
   return array;
