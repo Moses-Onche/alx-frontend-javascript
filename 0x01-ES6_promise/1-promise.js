cd6b969bf6a188f0ec4b4d700ec03ed5d615779f
Moses-Onche <onchemoses38@gmail.com>
2023-06-27 20:30:31 +0100

    Return a promise with a resolve using an object

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,6 +1,6 @@
-export default function (success) {
+export default function getResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    const object = {status: 200, body: 'Success'};
-    success ? resolve(object) : reject(Error('The fake API is not working currently'));
+    if (success) resolve({ status: 200, body: 'Success' });
+    else reject(new Error('The fake API is not working currently'));
   });
 }
