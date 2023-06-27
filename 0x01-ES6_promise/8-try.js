1d0e1c936437c08ffca224e6d459c56b9ce2401d
Moses-Onche <onchemoses38@gmail.com>
2023-06-27 22:30:33 +0100

    Throw new error if denominator is zero, else return division operation

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,4 +1,4 @@
-import default function divideFunction(numerator, denominator) {
+export default function divideFunction(numerator, denominator) {
   if (denominator === 0) throw new Error('cannot divide by 0');
   else return numerator / denominator;
 }
